0ccfe6dd0b654e184fbce986164dfeff49249062
Fagner Sales <fs.fagnersales@gmail.com>
2023-11-12 15:12:47 -0300

    sei lá

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import { Collection, Message } from 'discord.js'
 import { CreatingEmbed } from './structures/CreatingEmbed'
 
-
 export class App {
   public creatingEmbeds: Collection<Message['id'], CreatingEmbed> = new Collection()
 }
